c0f9e1da20c045f4310311084d105d26520fe828
princesssuna <1009suna@gmail.com>
2022-05-31 23:52:03 +0900

    Multicast Sender

---
+++ example/news_sender.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define TTL 64
+#define BUF_SIZE 30
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[]) {
+    int send_sock;
+    struct sockaddr_in mul_adr;
+    int time_live = TTL;
+    FILE *fp;
+    char buf[BUF_SIZE];
+
+    if(argc != 3) {
+        printf("Usage : %s <GroupIP> <PORT>\n", argv[0]);
+        exit(1);
+    }
+      
+    send_sock = socket(PF_INET, SOCK_DGRAM, 0); // UDP 소켓 생성
+    memset(&mul_adr, 0, sizeof(mul_adr));
+    mul_adr.sin_family = AF_INET;
+    mul_adr.sin_addr.s_addr = inet_addr(argv[1]);  // Multicast IP
+    mul_adr.sin_port = htons(atoi(argv[2]));       // Multicast Port
+    
+    // TTL 설정
+    setsockopt(send_sock, IPPROTO_IP, IP_MULTICAST_TTL, (void*)&time_live, sizeof(time_live));
+    
+    if((fp = fopen("news.txt", "r")) == NULL)
+        error_handling("fopen() error");
+
+    while(!feof(fp)) { /* Broadcasting */
+        fgets(buf, BUF_SIZE, fp);
+        sendto(send_sock, buf, strlen(buf), 0, (struct sockaddr*)&mul_adr, sizeof(mul_adr)); // 목적지가 특정 멀티캐스트 그룹 IP로 전송
+        sleep(2);
+    }
+    
+    fclose(fp);
+    close(send_sock);
+    return 0;
+}
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
